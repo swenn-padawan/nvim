13c90770bb1a66894d2076c89be9baa3abc18421
Swenn Tetrel <stetrel@2C1.42angouleme.fr>
2025-06-12 14:13:04 +0200

    🚧 wip: Add Toggle nvim tree remap

--- lua/swenn-padawan/remap.lua
+++ lua/swenn-padawan/remap.lua
@@ -3,3 +3,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set("n", "<leader>n", function()
+  require("nvim-tree.api").tree.toggle()
+end, { desc = "Toggle NvimTree" })
